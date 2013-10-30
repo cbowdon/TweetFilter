604c24b3cd6aff5a190a159c7ac8e4c582ad6356
Chris Bowdon <c.bowdon@bath.edu>
2013-10-30 23:18:54 +0000

    Tested downloading tweets with Main.

--- Main.hs
+++ Main.hs
@@ -2,21 +2,28 @@
 module Main where
 
 import Control.Monad
-import Data.Conduit
 import Network.HTTP.Conduit
+import Network.HTTP.Types.Header
 import TwitterTypes
 import Store
 import Store.Connection
 import Store.Test
 import Data.Aeson (eitherDecode)
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BC
 
 -- | Download tweets, store in database and generate Bayesian filter
 main :: IO ()
 main = do
-    token <- BL.readFile "/home/chris/Tweet/auth/bear_token.json"
-    content <- BL.readFile "/home/chris/Tweet/data/example.json"
-    html <- simpleHttp "https://www.startpage.com/"
-    putStrLn . take 100 . show $ html
-    print (eitherDecode token :: Either String Token)
-    print (eitherDecode content :: Either String Tweets)
+    eToken <- liftM eitherDecode $ BL.readFile "/home/chris/Tweet/auth/bear_token.json" :: IO (Either String Token)
+    case eToken of
+        Left err    -> print err
+        Right token -> do
+            req <- parseUrl "https://api.twitter.com/1.1/search/tweets.json?q=code&lang=en"
+            let req' = req {
+                requestHeaders = [(hAuthorization, BC.pack $ "Bearer " ++ accessToken token)]
+            }
+            -- print (requestHeaders req')
+            res <- withManager $ httpLbs req'
+            print res
+            print "Done"
