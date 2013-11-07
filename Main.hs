edd93faee052f23eee71ad662857f4467390866f
Chris Bowdon <c.bowdon@bath.edu>
2013-11-07 19:05:53 +0000

    Refactored main to use ErrorT.

--- Main.hs
+++ Main.hs
@@ -2,7 +2,6 @@
 -- | Download tweets, store in database and generate Bayesian filter
 module Main where
 
-import Control.Monad
 import Control.Monad.Error
 import Network.HTTP.Conduit
 import Network.HTTP.Types.Header
@@ -17,52 +16,27 @@ import qualified Data.ByteString.Char8 as BC
 tokenFile :: FilePath
 tokenFile = "/home/chris/Tweet/auth/bear_token.json"
 
+url :: String
+url = "https://api.twitter.com/1.1/search/tweets.json?q=code&lang=en"
+
 -- | Download tweets, store in database and generate Bayesian filter
 main :: IO ()
-main = do
-    eToken <- readToken tokenFile
-    eTweets <- download eToken
-    store eTweets
+main = eitherTweets >>= print
+    where
+        eitherTweets = runErrorT $ do
+            token <- readToken tokenFile
+            tweets <- download token
+            liftIO $ store tweets
+            return tweets
 
-main' = runErrorT $ do
-    token <- readToken' tokenFile
-    tweets <- download' token
-    liftIO $ store' tweets
-    return tweets
-
-readToken :: FilePath -> IO (Either String Token)
-readToken = liftM eitherDecode . BL.readFile
-
-download :: Either String Token -> IO (Either String Tweets)
-download eToken =
-    case eToken of
-        Left err    -> return $ Left err
-        Right token -> do
-            req <- parseUrl "https://api.twitter.com/1.1/search/tweets.json?q=code&lang=en"
-            let req' = req {
-                requestHeaders = [(hAuthorization, BC.pack $ "Bearer " ++ accessToken token)]
-            }
-            res <- withManager $ httpLbs req'
-            print res
-            let body = eitherDecode $ responseBody res :: Either String Tweets
-            return body
-
-store :: Either String Tweets -> IO ()
-store eTweets =
-    case eTweets of
-        Left err        -> print err
-        Right tweets    -> withConnection $ \conn -> do
-            mapM_ (insert conn) $ statuses tweets
-            print tweets
-
-readToken' :: FilePath -> ErrorT String IO Token
-readToken' filePath = ErrorT $ do
+readToken :: FilePath -> ErrorT String IO Token
+readToken filePath = ErrorT $ do
     contents <- BL.readFile filePath
     return $ eitherDecode contents
 
-download' :: Token -> ErrorT String IO Tweets
-download' token = ErrorT $ do
-    req <- liftIO $ parseUrl "https://api.twitter.com/1.1/search/tweets.json?q=code&lang=en"
+download :: Token -> ErrorT String IO Tweets
+download token = ErrorT $ do
+    req <- liftIO $ parseUrl url
     let req' = req {
         requestHeaders = [(hAuthorization, BC.pack $ "Bearer " ++ accessToken token)]
     }
@@ -70,8 +44,8 @@ download' token = ErrorT $ do
     liftIO $ print res
     return $ eitherDecode $ responseBody res
 
-store' :: Tweets -> IO ()
-store' tweets =
+store :: Tweets -> IO ()
+store tweets =
     withConnection $ \conn -> do
         mapM_ (insert conn) $ statuses tweets
         print tweets
